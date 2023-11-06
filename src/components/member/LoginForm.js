d361898190b904118824118120bf8f03f893963a
yonggyo1125 <yonggyo00@kakao.com>
2023-11-06 10:54:02 +0900

    수업 소스

--- src/components/member/LoginForm.js
+++ src/components/member/LoginForm.js
@@ -1,34 +1,47 @@
 import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
+import React, { useEffect } from 'react';
 
 const LoginForm = () => {
-  const { t, i18n } = useTranslation();
-  const [language, setLanguage] = useState('한국어');
+  const { t } = useTranslation();
+  let refUserId = React.createRef();
 
-  const handleLanguage = (e) => {
-    const el = e.currentTarget;
-    const lang = el.dataset.lang;
-    i18n.changeLanguage(lang);
-    setLanguage(lang === 'ko' ? '한국어' : 'English');
-  };
+  useEffect(() => {
+    /**
+     * current 속성을 통해서 DOM에 접근
+     */
+    //console.log(refUserId.current);
+    refUserId.current.focus();
+  }, [refUserId]);
 
+  /*
+  let refUserId;
+
+  useEffect(() => {
+    // 렌더링 직후, 갱신 직후 -> 렌더링 이후에 할 공통 작업
+    refUserId.focus();
+  }, [refUserId]);
+  */
   return (
     <>
       <h1>{t('로그인')}</h1>
-      <div>
-        {t('현재 언어')} : {language}
-      </div>
+
       <form>
-        <input type="text" name="userId" placeholder={t('아이디')} />
+        {/*
+        <input
+          type="text"
+          ref={(ref) => (refUserId = ref)}
+          name="userId"
+          placeholder={t('아이디')}
+  />*/}
+        <input
+          type="text"
+          name="userId"
+          placeholder={t('아이디')}
+          ref={refUserId}
+        />
         <input type="password" name="userPw" placeholder={t('비밀번호')} />
         <button type="submit">{t('로그인')}</button>
       </form>
-      <button type="button" onClick={handleLanguage} data-lang="ko">
-        한국어
-      </button>
-      <button type="button" onClick={handleLanguage} data-lang="en">
-        English
-      </button>
     </>
   );
 };
