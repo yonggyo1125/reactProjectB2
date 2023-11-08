c1f25a0431f232c07cdfdfb2ad66193885a9f443
yonggyo1125 <yonggyo00@kakao.com>
2023-11-08 10:35:28 +0900

    수업 소스

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,29 +2,18 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 const Posts = () => {
-  const [number, setNumber] = useState(0);
-  const [refresh, setRefresh] = useState(false);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    if (refresh) {
-      axios
-        .get('https://jsonplaceholder.typicode.com/posts')
-        .then((data) => {
-          console.log(data);
-          setRefresh(false);
-        })
-        .catch((err) => console.error(err));
-    }
-  }, [refresh]);
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-  return (
-    <>
-      <h1>{number}</h1>
-      <button type="button" onClick={() => setNumber(number + 1)}>
-        +1
-      </button>
-    </>
-  );
+  return <></>;
 };
 
 export default Posts;
