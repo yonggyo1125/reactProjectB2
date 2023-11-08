68d401b2da7c80ce1176093e6b64192002fee368
yonggyo1125 <yonggyo00@kakao.com>
2023-11-08 11:09:45 +0900

    .

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,13 +5,33 @@ const DECREASE = 'counter/decrease';
 const increase = () => ({ type: INCREASE });
 const decrease = () => ({ type: DECREASE });
 
-function reduce(state, action) {
+function reducer(state, action) {
   switch (action.type) {
     case INCREASE: // 증가 액션
+      return { number: state.number + 1 };
+
     case DECREASE: // 감소 액션
+      return { number: state.number - 1 };
+
+    default:
+      return state;
   }
 }
 
-const Counter = () => {};
+const Counter = () => {
+  // state : 현재 상태 값, dispatch : 액션을 발생시키는 함수
+  const [state, dispatch] = useReducer(reducer, { number: 0 });
+  return (
+    <>
+      <h1>{state.number}</h1>
+      <button type="button" onClick={() => dispatch(increase())}>
+        +1
+      </button>
+      <button type="button" onClick={() => dispatch(decrease())}>
+        -1
+      </button>
+    </>
+  );
+};
 
 export default Counter;
