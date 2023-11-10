79c405a8941f8662634c4072fea138764d05074c
yonggyo1125 <yonggyo00@kakao.com>
2023-11-10 11:52:29 +0900

    .

--- src/pages/board/BoardView.js
+++ src/pages/board/BoardView.js
@@ -1,7 +1,15 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 const BoardView = () => {
-  return <h1>게시글 보기..</h1>;
+  const { id } = useParams();
+  const location = useLocation();
+  console.log(location.search); // 쿼리스트링 값
+  return (
+    <>
+      <h1>게시글 보기..</h1>
+      <h2>게시글 번호 : {id}</h2>
+    </>
+  );
 };
 
 export default BoardView;
