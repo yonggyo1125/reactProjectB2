bbba4540553e31914ae52ec5a8bed471b4561abd
yonggyo1125 <yonggyo00@kakao.com>
2023-11-10 12:29:33 +0900

    .

---
+++ src/outlines/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <h1>푸터</h1>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/board/View.js
+++ src/pages/board/View.js
@@ -1,5 +1,14 @@
+import { useParams } from 'react-router-dom';
+
+const posts = {
+  [`post_100`]: '게시글1',
+  [`post_200`]: '게시글2',
+  [`post_300`]: '게시글3',
+};
 const View = () => {
-  return <h1>게시글 보기.....</h1>;
+  const { id } = useParams();
+
+  return <h1>{posts[`post_${id}`]}</h1>;
 };
 
 export default View;
